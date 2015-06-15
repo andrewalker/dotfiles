8d276e81b612ce51787208326e8423b6b254081e
andrewalker <andre@andrewalker.net>
2015-06-15 19:19:15 -0300

    Comment, improve shell scripts

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,1 +1,2 @@
+# Old servers don't know anything about screen-256color
 alias ssh="TERM=xterm ssh"
--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,1 +1,5 @@
-eval `gnome-keyring-daemon --start`
+# TODO: how do I really know when to execute this? This "if" should be
+# improved.
+if [ -x /usr/bin/gnome-keyring-daemon ]; then
+    eval `gnome-keyring-daemon --start`
+fi
