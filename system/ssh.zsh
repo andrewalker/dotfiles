c94c020a16eaf8365857a717da7dafcef60addb4
andrewalker <andre@andrewalker.net>
2015-07-15 18:35:10 -0300

    Improve gnome agent integration

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,5 +1,6 @@
 # TODO: how do I really know when to execute this? This "if" should be
 # improved.
-if [ -x /usr/bin/gnome-keyring-daemon ]; then
+if [[ -x /usr/bin/gnome-keyring-daemon && -n "$DESKTOP_SESSION" ]]; then
     eval `gnome-keyring-daemon --start`
+    export SSH_AUTH_SOCK
 fi
