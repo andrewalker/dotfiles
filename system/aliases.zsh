ac6f0e80beab862ca306fa4a7c5df381919afea9
André Walker <andre@andrewalker.net>
2015-09-05 22:40:04 -0300

    Create helper function to mount usb

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,2 +1,15 @@
 # Old servers don't know anything about screen-256color
 alias ssh="TERM=xterm ssh"
+
+function mount-usb {
+    device=$1
+    target=$2
+
+    if [ -z "$target" ]; then
+        target='/mnt/usb'
+    fi
+
+    sudo mkdir -p $target && \
+    sudo chmod 777 $target && \
+    sudo mount -o rw,uid=`id -u` $device $target
+}
