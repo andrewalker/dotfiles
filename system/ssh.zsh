ee916e94f1fa504876fe353a397d2730159fee29
André Walker <andre@andrewalker.net>
2015-09-05 22:39:37 -0300

    Use keychain outside gnome

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,6 +1,8 @@
 # TODO: how do I really know when to execute this? This "if" should be
 # improved.
-if [[ -x /usr/bin/gnome-keyring-daemon && -n "$DESKTOP_SESSION" ]]; then
+if [[ -x /usr/bin/gnome-keyring-daemon && "$DESKTOP_SESSION" == "gnome" ]]; then
     eval `gnome-keyring-daemon --start`
     export SSH_AUTH_SOCK
+else
+    eval `keychain --eval --quick --quiet --agents ssh id_rsa github_rsa`
 fi
