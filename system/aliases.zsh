2715c3949acd84edaa562a0f1cca39112b33e3f7
Andre Walker <andre.walker@booking.com>
2016-06-01 17:05:02 +0200

    I don't even...

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,30 @@
 # Old servers don't know anything about screen-256color
-alias ssh="TERM=xterm ssh"
+# alias ssh="TERM=xterm ssh"
+
+if [ -f '/usr/local/bin/grm' ]
+then
+    alias rm='/usr/local/bin/grm -I'
+fi
+alias sshbk="ssh -o 'ProxyCommand ssh -qx -W %h:%p shell'"
+alias cl="$HOME/bin/dehilight"
+alias mailwork="mutt -F ~/.mutt/muttrc-work"
+alias mailpersonal="mutt -F ~/.mutt/muttrc-personal"
+
+function syncmail {
+    minutes=$1
+    while [ 1 ]
+    do
+        mbsync booking
+        mbsync fastmail
+        notmuch-poll-work
+        echo "Sleeping $minutes minutes"
+        sleep $( expr $minutes \* 60 )
+    done
+}
+
+function open-eml {
+    formail -b < $1 > out.mbox && mutt -f out.mbox && rm out.mbox
+}
 
 function mount-usb {
     device=$1
--- system/ssh.zsh
+++ system/ssh.zsh
@@ -3,6 +3,6 @@
 if [[ -x /usr/bin/gnome-keyring-daemon && "$DESKTOP_SESSION" == "gnome" ]]; then
     eval `gnome-keyring-daemon --start`
     export SSH_AUTH_SOCK
-else
-    eval `keychain --eval --quick --quiet --agents ssh id_rsa github_rsa`
+elif [[ -x /usr/bin/keychain ]]; then
+   eval `keychain --eval --quick --quiet --agents ssh id_rsa github_rsa`
 fi
