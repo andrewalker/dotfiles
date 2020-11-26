11f43797987605b5cf35cd86ce7a799761fddc0e
Andre Walker <andre.walker@booking.com>
2020-11-26 20:10:24 +0100

    Be less verbose

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if test ! -f ~/.vim/autoload/plug.vim
+then
+    curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-vim --not-a-term -c "PlugInstall" -c "quitall" >/dev/null
+# best effort! if there's an error, I can retry to install plugins later
+vim --not-a-term -c "PlugInstall" -c "quitall" >/dev/null 2>/dev/null
