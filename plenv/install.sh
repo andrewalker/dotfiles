659b57d4394e72cfafd8828b468139d99a1195c0
Andre Walker <andre.walker@booking.com>
2020-11-26 20:17:35 +0100

    Rework plenv to use submodules

--- plenv/install.sh
+++ plenv/install.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if [ ! -d $HOME/.plenv ]; then
-    git clone git://github.com/tokuhirom/plenv.git ~/.plenv
-else
-    cd $HOME/.plenv
-    git pull
-    cd -
-fi
-
-if [ ! -d $HOME/.plenv/plugins/perl-build/ ]; then
-    git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
-else
-    cd $HOME/.plenv/plugins/perl-build
-    git pull
-    cd -
-fi
+this_dir=$(dirname $0)
+mkdir -p ~/.plenv/plugins/
+ln -sf $this_dir/plugin-perl-build ~/.plenv/plugins/perl-build
