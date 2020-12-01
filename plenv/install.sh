92f69eb102d6db835c1d21c2bc49505a41308f0c
Andre Walker <andre.walker@booking.com>
2020-12-01 11:23:53 +0100

    Handle symlinks in a safer way

--- plenv/install.sh
+++ plenv/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -eu
+
 this_script=$(readlink -f $0);
 this_dir=$(dirname $this_script);
 mkdir -p ~/.plenv/plugins/
-ln -sf $this_dir/plugin-perl-build ~/.plenv/plugins/perl-build
+ln -sf -T $this_dir/plugin-perl-build ~/.plenv/plugins/perl-build
