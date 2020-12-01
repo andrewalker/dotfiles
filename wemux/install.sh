88c2b59afc7909436e3f9e6b15f63501340161bc
Andre Walker <andre.walker@booking.com>
2020-12-01 14:30:35 +0100

    Oops

--- wemux/install.sh
+++ wemux/install.sh
@@ -4,5 +4,5 @@ set -eu
 
 this_script=$(readlink -f $0);
 this_dir=$(dirname $this_script);
-sudo cp $this_script /usr/local/bin/wemux
+sudo cp $this_dir/wemux /usr/local/bin/wemux
 sudo cp $this_dir/wemux.conf /usr/local/etc/wemux.conf
