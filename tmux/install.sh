d8b718343efa6c95e45c43f9175c4616e94e2b36
Andre Walker <andre@andrewalker.net>
2024-02-14 17:29:15 +0100

    Cleanup old stuff, add nord

--- tmux/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-this_script=$(readlink -f $0);
-this_dir=$(dirname $this_script);
-mkdir -p ~/.tmux/plugins
-ln -sf -T $this_dir/plugin-tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
