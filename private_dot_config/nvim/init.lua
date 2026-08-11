015bcf8ee940f52d0690e2aded33e8b2a4087e24
André Walker <andre@andrewalker.net>
2026-08-11 23:22:20 +0200

    Actually make remember plugin work

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -133,7 +133,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-  "vladdoster/remember.nvim",
+  { "vladdoster/remember.nvim", opts = {} },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
